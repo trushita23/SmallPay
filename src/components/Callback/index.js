8a6f62c9a2a4e6f978ef2a6d0030d041ac6e03e3
Trushita Maurya <trushita@gmail.com>
2025-06-20 00:59:06 -0400

    callback component

---
+++ src/components/Callback/index.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { useAuth } from "react-oidc-context";
+import { useHistory } from "react-router-dom";
+
+const Callback = () => {
+  const auth = useAuth();
+  const history = useHistory();
+
+  useEffect(() => {
+    // Manually trigger sign-in callback processing
+    auth.signinCallback().then(() => {
+      history.replace("/"); // redirect after successful login
+    });
+  }, []);
+
+  return <div>Signing in...</div>;
+};
+
+export default Callback;
