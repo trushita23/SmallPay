ea6acdddac5810af6ed97f344a31e233b85e3248
Nishit Mistry <ns209086@dal.ca>
2025-06-23 21:59:10 -0700

    Adding lambda codes

---
+++ lambda-codes/triggerSQS.js
@@ -0,0 +1,26 @@
+var AWS = require("aws-sdk");
+var sqs = new AWS.SQS();
+var SQS_URL = "";
+
+exports.handler = async (event) => {
+  const req = JSON.stringify(event.invoiceNumber);
+  var params = {
+    DelaySeconds: 2,
+    MessageAttributes: {
+      invoiceNumber: {
+        DataType: "Number",
+        StringValue: req,
+      },
+    },
+    MessageBody: req,
+    QueueUrl: `${SQS_URL}/generatePdfQueue`,
+  };
+
+  let queueRes = await sqs.sendMessage(params).promise();
+  const response = {
+    statusCode: 200,
+    body: queueRes,
+  };
+
+  return response;
+};
